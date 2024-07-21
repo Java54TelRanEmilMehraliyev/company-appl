07cee38f210e1f653ff9ba7563d65f0ee474b5eb
user.emial <emil.mehraliyev003@gmail.com>
2024-07-21 19:54:23 +0300

    Completed HW 38

---
+++ src/main/java/telran/employees/CompanyAppl.java
@@ -0,0 +1,31 @@
+package telran.employees;
+
+import telran.io.Persistable;
+import terlan.view.Item;
+import terlan.view.Menu;
+import terlan.view.SystemInputOutput;
+
+import java.util.*;
+public class CompanyAppl {
+
+	private static final String FILE_NAME = "employeesTest.data";
+
+	public static void main(String[] args) {
+		Company company = new CompanyMapsImpl();
+		try {
+			((Persistable)company).restore(FILE_NAME);
+		} catch (Exception e) {
+			
+		}
+		List<Item> companyItems =
+				CompanyApplItems.getCompanyItems(company,
+						new HashSet<String>(List.of("Audit", "Development", "QA")));
+		companyItems.add(Item.of("Exit & save",
+				io -> ((Persistable)company).save(FILE_NAME), true));
+		Menu menu = new Menu("Company CLI Application",
+				companyItems.toArray(Item[]::new));
+		menu.perform(new SystemInputOutput());
+
+	}
+
+}
